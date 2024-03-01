f85052a3713d358f3557da5b6ff551ceade5a6f2
BigenteAlfa <bigentealfa@gmail.com>
2024-03-01 19:56:21 -0300

    error solved

--- submit_reddit_chile.js
+++ submit_reddit_chile.js
@@ -8,7 +8,7 @@ export async function submitRedditChile(req, res) {
         const redditPosts = await fetchDataReddit(subreddits);
         await insertarDatosReddit(redditPosts);
         console.log('Script ejecutado correctamente.');
-        return res.status(200).send('Datos de Reddit insertados correctamente en la base de datos.');
+        //return res.status(200).send('Datos de Reddit insertados correctamente en la base de datos.');
     } catch (error) {
         console.error('Error en el proceso:', error);
         //res.status(500).send('Error en el proceso.');
