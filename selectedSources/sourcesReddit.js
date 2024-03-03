31b37ea1c874e5ad201f5c0dd131f8905bd8d4e5
BigenteAlfa <bigentealfa@gmail.com>
2024-03-03 18:32:34 -0300

    create external subreddit file

---
+++ selectedSources/sourcesReddit.js
@@ -0,0 +1,8 @@
+// Subreddits seleccionados en cada tópico
+
+const topicsReddit = {
+    chile: ['chile', 'RepublicadeChile', 'Santiago', 'ChileIT', 'Chilefit'],
+    energy: []
+};
+
+export default topicsReddit;
