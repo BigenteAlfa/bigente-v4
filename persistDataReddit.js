6dc2bc26c8f7faf8fe03ce8d7e30f20bf40f4e24
BigenteAlfa <bigentealfa@gmail.com>
2024-03-04 21:18:31 -0300

    add time_added to SQL query

--- persistDataReddit.js
+++ persistDataReddit.js
@@ -11,10 +11,10 @@ async function persistDataReddit(posts) {
         await Promise.all(posts.map(async (listing) => {
             await Promise.all(listing.map(async (post) => {
                 const query = `
-                    INSERT INTO db_reddit_chile (id, title, subreddit, selftext, thumbnail, score, url, time_created)
-                    VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+                    INSERT INTO db_reddit_chile (id, title, subreddit, selftext, thumbnail, score, url, time_created, time_added)
+                    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, NOW() AT TIME ZONE 'America/Santiago')
                     ON CONFLICT (id) DO UPDATE
-                    SET score = EXCLUDED.score, time_edited = CURRENT_TIMESTAMP AT TIME ZONE 'UTC' AT TIME ZONE '-03'
+                    SET score = EXCLUDED.score, time_edited = CURRENT_TIMESTAMP AT TIME ZONE 'UTC' AT TIME ZONE 'America/Santiago'
                     RETURNING time_edited;
                 `;
                 const values = [post.id, post.title, post.subreddit, post.selftext, post.thumbnail, post.score, post.url, post.created_utc];
